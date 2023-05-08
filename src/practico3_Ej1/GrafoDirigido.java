047e971098acf7122f6fcef629909b27eff9b586
Hilario <heyler@alumnos.exa.unicen.edu.ar>
2023-05-08 17:54:17 -0300

    Implementacion clase GrafoDirigido en proceso

--- src/practico3_Ej1/GrafoDirigido.java
+++ src/practico3_Ej1/GrafoDirigido.java
@@ -3,47 +3,65 @@ package practico3_Ej1;
 import java.util.*;
 
 public class GrafoDirigido<T> implements Grafo<T> {
-	private Map<Integer, List<T>> mapa = new HashMap<>();
+	private Map<Integer, List<Arco<T>>> mapa = new HashMap<>();
 
 	@Override
 	public void agregarVertice(int verticeId) {
-		// TODO Auto-generated method stub
-		
+		this.mapa.put(verticeId, new LinkedList<Arco<T>>()); //Polimorfismo repaso prog2
 	}
 
 	@Override
 	public void borrarVertice(int verticeId) {
-		// TODO Auto-generated method stub
-		
+		this.mapa.remove(verticeId);
 	}
 
 	@Override
 	public void agregarArco(int verticeId1, int verticeId2, T etiqueta) {
-		// TODO Auto-generated method stub
+		if(!mapa.containsKey(verticeId1)) {
+			this.agregarVertice(verticeId1);
+		}
 		
+		if(!mapa.containsKey(verticeId2)) {
+			this.agregarVertice(verticeId2);
+		}
+		
+		Arco<T> nuevoArco = new Arco<T>(verticeId1, verticeId2, etiqueta);
+		
+		if(!mapa.get(verticeId1).contains(nuevoArco)) {//TODO Habria que redefinir el equals de Arco y especificar cuando dos arcos son considerados iguales
+			mapa.get(verticeId1).add(nuevoArco); 
+		}
 	}
 
 	@Override
-	public void borrarArco(int verticeId1, int verticeId2) {
-		// TODO Auto-generated method stub
-		
+	public void borrarArco(int verticeId1, int verticeId2) {//Complejidad O(n^2) o O(n)? creo que O(n) porque no tiene iteradores anidados.
+		//this.mapa.get(verticeId1) retorna la lista de arcos a los que apunta el vertice especificado en el parametro
+		List<Arco<T>> arcosAsociados = this.mapa.get(verticeId1); 
+		for(int i = 0; i < arcosAsociados.size(); i++) {
+			if(arcosAsociados.get(i).getVerticeDestino() == verticeId2) {
+				arcosAsociados.remove(i);
+			}
+		}
 	}
 
 	@Override
 	public boolean contieneVertice(int verticeId) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.mapa.containsKey(verticeId);
 	}
 
 	@Override
 	public boolean existeArco(int verticeId1, int verticeId2) {
-		// TODO Auto-generated method stub
+		List<Arco<T>> arcosAsociados = this.mapa.get(verticeId1); 
+		for(int i = 0; i < arcosAsociados.size(); i++) {
+			if(arcosAsociados.get(i).getVerticeDestino() == verticeId2) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
 	@Override
 	public Arco<T> obtenerArco(int verticeId1, int verticeId2) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
